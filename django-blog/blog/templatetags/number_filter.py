bf75f9098027ec989b18bbd3d1c6dd5f72e95b89
Andrey Knyazev <knyazev.andr99@gmail.com>
2023-07-18 19:16:37 +0300

    formatting

--- django-blog/blog/templatetags/number_filter.py
+++ django-blog/blog/templatetags/number_filter.py
@@ -11,7 +11,7 @@ def num_to_text(val):
     pattern = r'(\d+)|(<[^>]*>)'
 
     return re.sub(pattern,
-    lambda match: num2words(int(
-        match.group(0)), lang='ru') if match.group(1) else match.group(2),
-        val
-    )
+                  lambda match: num2words(int(
+                      match.group(0)), lang='ru') if match.group(1) else match.group(2),
+                  val
+                  )

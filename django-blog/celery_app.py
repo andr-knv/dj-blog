10c9196a98dceaeb693cb4a9b9ebf98fb0e1db88
Andrey Knyazev <knyazev.andr99@gmail.com>
2023-07-21 19:14:55 +0300

    ...

---
+++ django-blog/celery_app.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_blog.settings")
+
+app = Celery('dj_blog')
+app.config_from_object('django.conf:settings')
+app.conf.broker_url = settings.CELERY_BROKER_URL
+app.autodiscover_tasks()
+
